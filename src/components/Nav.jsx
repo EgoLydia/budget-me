2e3e6afdb43e19d152df6246a0bd06d0c48527ef
Lydia Ego <egolydia97@gmail.com>
2023-03-07 14:10:22 +0100

    Feat: nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,15 @@
+import { Form, NavLink } from 'react-router-dom'
+import logomark from '../assets/logomark.svg'
+
+import { TrashIcon } from '@heroicons/react/24/solid'
+
+const Nav = ({ userName }) => {
+    return (
+        <nav>
+            <NavLink to='/' aria-label='Go to home'>
+                <img src={logomark} alt="" height={30} />
+                <span>BudgetMe</span>
+            </NavLink>
             {
                 userName && (
                     <Form method='post' action='/logout' onSubmit={(event) => {
@@ -11,3 +23,9 @@
                         </button>
                     </Form>
                 )
+            }
+        </nav>
+    )
+}
+
+export default Nav
