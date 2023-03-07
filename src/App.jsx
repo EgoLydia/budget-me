7e483459b022f4676e25b2a512a9c95bcc236a05
Lydia Ego <egolydia97@gmail.com>
2023-03-07 14:15:20 +0100

    Feat: registered required routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        action: dashboardAction,
+        errorElement: <Error />
+      },
+      {
+        path: "logout",
+        action: logoutAction
+      }
+    ]
+  },
+
 function App() {
 
   return (
