cb6bcc81baa5dd642be0ae257c8f84428a0ec86d
Lydia Ego <egolydia97@gmail.com>
2023-03-13 16:02:50 +0100

    Feat: added isSubmitting loading state when a new budget is created

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -1,14 +1,27 @@
 import { CurrencyDollarIcon } from '@heroicons/react/24/solid'
-import React from 'react'
-import { Form } from 'react-router-dom'
+import React, { useEffect, useRef } from 'react'
+import { Form, useFetcher } from 'react-router-dom'
 
 const AddBudgetForm = () => {
+    const fetcher = useFetcher();
+    const isSubmitting = fetcher.state === "submitting";
+
+    const formRef = useRef();
+    const focusRef = useRef();
+
+    useEffect(() => {
+        if (!isSubmitting) {
+            formRef.current.reset();
+            formRef.current.focus();
+        }
+    }, [isSubmitting]);
+
     return (
         <div className='form-wrapper'>
             <h2 className="h3">
                 Create Budget
             </h2>
-            <Form method='post' className='grid-sm'>
+            <fetcher.Form method='post' className='grid-sm' ref={formRef}>
                 <div className="grid-xs">
                     <label htmlFor="newBudget">Budget Name</label>
                     <input
@@ -17,24 +30,32 @@ const AddBudgetForm = () => {
                         id='newBudget'
                         placeholder='e.g. Groceries'
                         required
+                        ref={focusRef}
                     />
                 </div>
                 <div className="grid-xs">
                     <label htmlFor="newBudgetAmount">Budget Amount</label>
                     <input
-                        type="text"
+                        type="number"
                         name='newBudgetAmount'
                         id='newBudgetAmount'
-                        placeholder='e.g., $350'
+                        placeholder='e.g., NGN 3000'
                         required
                         inputMode='decimal'
                     />
                 </div>
-                <button type='submit' className='btn btn--dark'>
-                    <span>Create budget</span>
-                    <CurrencyDollarIcon width={20} />
+                <input type="hidden" name='_action' value="createBudget" />
+                <button type='submit' className='btn btn--dark' disabled={isSubmitting}>
+                    {
+                        isSubmitting ? <span>Submitting....</span> : (
+                            <>
+                                <span>Create budget</span>
+                                <CurrencyDollarIcon width={20} />
+                            </>
+                        )
+                    }
                 </button>
-            </Form>
+            </fetcher.Form>
         </div>
     )
 }
