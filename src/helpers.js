2abe576a1f0ecdc4f811d972cbf62ee2d2038aaa
Lydia Ego <egolydia97@gmail.com>
2023-03-13 16:05:16 +0100

    Feat: helpers to create budgets and expenses

--- src/helpers.js
+++ src/helpers.js
@@ -8,5 +8,32 @@ export const fetchData = (key) => {
 };
 
 export const deleteItem = ({ key }) => {
+//create budget
+export const createBudget = ({ name, amount }) => {
+    const newItem = {
+        id: crypto.randomUUID(),
+        name: name,
+        createdAt: Date.now(),
+        amount: +amount,
+        color: generateRandColor()
+    }
+    const existingBudgets = fetchData("budgets") ?? [];
+
+    return localStorage.setItem("budgets", JSON.stringify([...existingBudgets, newItem]))
+}
+
+//create expense
+export const createExpense = ({ name, amount, budgetId }) => {
+    const newItem = {
+        id: crypto.randomUUID(),
+        name: name,
+        createdAt: Date.now(),
+        amount: +amount,
+        budgetId: budgetId
+    }
+    const existingExpenses = fetchData("expenses") ?? [];
+
+    return localStorage.setItem("expenses", JSON.stringify([...existingExpenses, newItem]))
+}
     return localStorage.removeItem(key)
 }
