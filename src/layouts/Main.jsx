51df7b01f0ec6111f942f0f587b9817873fb8242
Lydia Ego <egolydia97@gmail.com>
2023-03-13 16:00:36 +0100

    Feat: main page

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -4,3 +4,23 @@ import { fetchData } from "../helpers"
 import wave from '../assets/wave.svg'
 import Nav from "../components/Nav";
 
+export function mainLoader() {
+    const userName = fetchData('userName');
+    return { userName }
+}
+
+const Main = () => {
+    const { userName } = useLoaderData()
+    return (
+
+        <div className="layout">
+            <Nav userName={userName} />
+            <main>
+                <Outlet />
+            </main>
+            <img src={wave} alt="" />
+        </div>
+    )
+}
+
+export default Main
